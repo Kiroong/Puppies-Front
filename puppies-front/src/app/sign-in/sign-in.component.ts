cc6385d9b3e125edbb98074191349ea7a997e1d6
javelinsman <javelinsman@snu.ac.kr>
2019-04-23 22:02:11 +0900

    백엔드 주소 env에 설정하고 백엔드랑 소통

--- puppies-front/src/app/sign-in/sign-in.component.ts
+++ puppies-front/src/app/sign-in/sign-in.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-sign-in',
@@ -13,10 +14,13 @@ export class SignInComponent implements OnInit {
   ) { }
 
   dogPhotoUrl: string;
+  backendUrl = environment.backendUrl;
 
   ngOnInit() {
     this.http.get<any>('https://dog.ceo/api/breed/Shiba/images/random')
       .subscribe(data => this.dogPhotoUrl = data.message);
+    this.http.get<any>(this.backendUrl)
+      .subscribe(data => console.log(data));
   }
 
 }
